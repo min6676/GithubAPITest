74702407d0491240be65cfc609e162dba3382d27
butga@naver.com <butga@naver.com>
2021-10-29 17:41:54 +0900

    First commit

---
+++ GithubAPITest/UserCell.swift
@@ -0,0 +1,26 @@
+//
+//  UserCell.swift
+//  GithubAPITest
+//
+//  Created by 김민순 on 2021/10/29.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        profileImageView.layer.cornerRadius = profileImageView.frame.height / 2
+        profileImageView.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
